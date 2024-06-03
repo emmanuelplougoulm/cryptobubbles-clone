f05f845e83857ceb10dd60818aa2b369d4f1976b
viktor_vaughn <emmanuel.plougoulm@gmail.com>
2024-06-03 19:55:19 +0200

    launch app on port 5000

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,15 @@
 // https://ajaynjain.medium.com/create-react-app-to-vite-the-ultimate-guide-5153be43a015
 
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig, } from "vite";
 import plugins from "./plugins";
 
 export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), ["VITE_"]);
 
 	return {
 		plugins: plugins(mode),
 		server: {
-			port: 3000, // To run the app on port 3000
-			open: true, // If we want to open the app once its started
+			port: 3000,
+			open: true,
 		},
 	};
 });
-
-
-
-
