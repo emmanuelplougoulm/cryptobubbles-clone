6dded0ce08a44b8725ed2cac2f19261df5b065c3
viktor_vaughn <emmanuel.plougoulm@gmail.com>
2024-06-11 15:19:48 +0200

    [bubbles]: rm useless components

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface Props {}
-
-const Footer: React.FC<Props> = (props) => {
-  return <div></div>;
-};
-
-export default Footer;
--- src/components/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-import type React from "react";
-
-type Props = {}
-
-const Navbar: React.FC<Props> = (props) => {
-  return <div></div>;
-};
-
-export default Navbar;
